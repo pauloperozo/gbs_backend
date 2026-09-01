a12b35edc34bdd7a4dcf76d36ef1491c8e665006
pauloperozocolombia@gmail.com <pauloperozocolombia@gmail.com>
2026-09-01 03:24:41 -0500

    refactor: assign port to variable before starting application listener

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document, swaggerServerlessOptions);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
